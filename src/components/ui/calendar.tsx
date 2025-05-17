ec1e3ee662732eb022ec083f00ffdf2f3232108c
ToquiNovic <toquicaagudelo123@gmail.com>
2025-05-17 14:29:15 -0500

    refactor: comment out unused icon imports and remove unsupported icon overrides in Calendar component

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+// import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -57,14 +57,7 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
-      }}
+      // Removed unsupported PrevIcon and NextIcon overrides
       {...props}
     />
   )
